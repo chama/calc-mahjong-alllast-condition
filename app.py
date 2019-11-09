d3752fb39727ecc3ba298e306ba021911e7802ef
chama <rk12381995@gmail.com>
2019-11-09 19:13:21 +0900

    供託表示が無いことを修正

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', titile='calc')
+    return render_template('index.html', title='calc', message='オーラスの条件計算をします。')
 
+@app.route('/', methods=['POST'])
+def form():
+    m_point = request.form.get('my_point')
+    e_point = request.form.get('en_point')
+    kyoutaku = request.form.get('kyoutaku')
+    honba = request.form.get('honba')
+    tumibou = request.form.get('tumibou')
+    return render_template('index.html', title='calc', message=[m_point, e_point, kyoutaku, honba, tumibou])
 
 
 
